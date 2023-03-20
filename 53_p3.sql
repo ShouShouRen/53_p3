3034576df1e50f8bd71c9fef4f5c48b879e03100
Peter Chen <peterchen2017125@gmail.com>
2023-03-20 11:06:16 +0800

    20230320

--- 53_p3.sql
+++ 53_p3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-03-19 14:37:32
+-- 產生時間： 2023-03-20 04:05:44
 -- 伺服器版本： 10.4.27-MariaDB
 -- PHP 版本： 8.2.0
 
@@ -48,6 +48,31 @@ INSERT INTO `login_log` (`user`, `login_time`, `status`) VALUES
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `product_des` text NOT NULL,
+  `price` varchar(50) NOT NULL,
+  `links` varchar(100) NOT NULL,
+  `time` datetime NOT NULL,
+  `images` varchar(100) NOT NULL,
+  `template` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `products`
+--
+
+INSERT INTO `products` (`id`, `product_name`, `product_des`, `price`, `links`, `time`, `images`, `template`) VALUES
+(1, '咖啡商品01', '咖啡好喝', '100', 'coffee.com', '2023-03-20 11:03:32', 'ea4c6ed619391d1a7e658e09221558d0.jpg', '1'),
+(2, '咖啡商品02', '好喝的咖啡', '200', 'coffee.com', '2023-03-20 11:04:41', 'c45181e604ad8ddfe1560a863f0d9d03.jpg', '2');
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `users`
 --
 
@@ -76,6 +101,12 @@ INSERT INTO `users` (`id`, `user_id`, `user`, `pw`, `user_name`, `role`) VALUES
 --
 
 --
+-- 資料表索引 `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `users`
 --
 ALTER TABLE `users`
@@ -84,6 +115,12 @@ ALTER TABLE `users`
 --
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `users`
